02790c94ee90973e7a259e24abe351618acc3c41
Lucas Boeing Scarduelli <lucas@scarduelli.com>
2018-04-20 14:00:01 -0300

    Bug fix transaction route

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,9 +1,11 @@
-'use strict';
+'use strict'
 
-const routerAccount = require('./account');
-const routerCategory = require('./category');
-const routerCreditCard = require('./creditCard');
+const routerAccount = require('./account')
+const routerCategory = require('./category')
+const routerCreditCard = require('./creditCard')
+const routerTransaction = require('./transaction')
 
-module.exports.account = routerAccount;
-module.exports.category = routerCategory;
-module.exports.creditCard = routerCreditCard;
+module.exports.account = routerAccount
+module.exports.category = routerCategory
+module.exports.creditCard = routerCreditCard
+module.exports.transaction = routerTransaction
