054e39e3af6860167e2a4d85079346a8120003e1
Tiago G. Ribeiro <tiago.sistema@yahoo.com>
2018-04-30 15:51:51 -0300

    Refactoring to linter 'standard'

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
+require('dotenv').config()
 
-require('./db/index');
+require('./db/index')
 
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const md5 = require('md5');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+// const md5 = require('md5')
 
-const routes = require('./routes/index');
-const apiV1 = require('./routes/v1/index');
+const routes = require('./routes/index')
+const apiV1 = require('./routes/v1/index')
 
-var app = express();
-app.locals.dateFormat= require('dateformat');
-app.locals.currencyFormatter = require('currency-formatter');
+var app = express()
+app.locals.dateFormat = require('dateformat')
+app.locals.currencyFormatter = require('currency-formatter')
 
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(logger('dev'))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
-app.use('/', routes);
-app.use('/api/v1/accounts', apiV1.account);
-app.use('/api/v1/categories', apiV1.category);
-app.use('/api/v1/credit-cards', apiV1.creditCard);
-app.use('/api/v1/transactions', apiV1.transaction);
+app.use('/', routes)
+app.use('/api/v1/accounts', apiV1.account)
+app.use('/api/v1/categories', apiV1.category)
+app.use('/api/v1/credit-cards', apiV1.creditCard)
+app.use('/api/v1/transactions', apiV1.transaction)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res, next) => {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leakeiniciard to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
