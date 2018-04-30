99017f03643488db12dc5f1d142d8321bf72f11c
Tiago G. Ribeiro <tiago.sistema@yahoo.com>
2018-04-30 15:21:37 -0300

    Refactoring in codes
    Refactoring in codes of configurations, is necessary modified the
    structure to aplicated the pattern 'standard'

--- db/index.js
+++ db/index.js
@@ -1,28 +1,28 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
-const dbURI = process.env.DB_URI;
-mongoose.connect(dbURI);
+const mongoose = require('mongoose')
+const dbURI = process.env.DB_URI
+mongoose.connect(dbURI)
 
-mongoose.connection.on('connected', function() {
-    console.log('Mongoose default connection connected to ' + dbURI);
-});
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose default connection connected to ${dbURI}`)
+})
 
-mongoose.connection.on('error', function(err) {
-    console.log('Mongoose default connection error: ' + err);
-});
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose default connection error:  ${err}`)
+})
 
-mongoose.connection.on('disconnected', function() {
-    console.log('Mongoose default connection disconnected');
-});
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose default connection disconnected`)
+})
 
-mongoose.connection.on('open', function() {
-    console.log('Mongoose default connection is open');
-});
+mongoose.connection.on('open', () => {
+  console.log(`Mongoose default connection is open`)
+})
 
-process.on('SIGINT', function() {
-    mongoose.connection.close(function() {
-        console.log('Mongoose defaut connection disconnected through app termination');
-        process.exit(0);
-    })
-});
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log(`Mongoose defaut connection disconnected through app termination`)
+    process.exit(0)
+  })
+})
